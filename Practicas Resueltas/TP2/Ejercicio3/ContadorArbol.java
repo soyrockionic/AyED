4d59c0e1b90cd0875c04a2e2c747faedac0fd142
Juan <soyrockionic@gmail.com>
2026-04-14 02:16:25 -0300

    Actualizo ejercicio 3

--- Practicas Resueltas/TP2/Ejercicio3/ContadorArbol.java
+++ Practicas Resueltas/TP2/Ejercicio3/ContadorArbol.java
@@ -1,49 +1,77 @@
 package TP2.Ejercicio3;
 
-import TP2.Ejercicio1.BinaryTree;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.List;
+import TP2.Ejercicio1.BinaryTree;
 
 public class ContadorArbol {
     
     private BinaryTree<Integer> arbol = new BinaryTree<>();
     
-    public ContadorArbol(BinaryTree<Integer> controlador){
+    public ContadorArbol (BinaryTree<Integer> controlador) {
         this.arbol = controlador;
     }
     
-    public LinkedList<Integer> numerosPares(){
-        List<Integer> pares = new LinkedList<>();
-        postOrden(arbol, pares);
-        return (LinkedList<Integer>) pares;
+    public List<Integer> numerosPares() {
+        List<Integer> pares = new ArrayList<>();
+        PostOrden(arbol,pares);
+        return pares;
     }
     
     // Recorrido InOrden (Izquierda - Raiz - Derecha)
-    private void inOrden(BinaryTree<Integer> nodo, List<Integer> pares) {
-        if (nodo == null) return;
-        try {
-            inOrden(nodo.getLeftChild(), pares);
-        } catch (IllegalStateException e) {}
-        if (nodo.getData() != null && nodo.getData() % 2 == 0) {
-            pares.add(nodo.getData());
+    private void InOrden(BinaryTree<Integer> nodo, List<Integer> pares) {
+        if (nodo.isEmpty()) return;
+        
+        if (nodo.hasLeftChild()) {
+            InOrden(nodo.getLeftChild(), pares);
         }
-        try {
-            inOrden(nodo.getRightChild(), pares);
-        } catch (IllegalStateException e) {}
+    
+        Integer dato = nodo.getData();
+        if (dato != null && dato % 2 == 0) {
+            pares.add(dato);
+        }
+    
+        if (nodo.hasRightChild()) {
+            InOrden(nodo.getRightChild(), pares);
+        }
     }
     
     // Recorrido PostOrden (Izquierda - Derecha - Raiz)
-    private void postOrden(BinaryTree<Integer> nodo, List<Integer> pares) {
+    private void PostOrden(BinaryTree<Integer> nodo, List<Integer> pares) {
         if (nodo == null) return;
-        try {
-            postOrden(nodo.getLeftChild(), pares);
-        } catch (IllegalStateException e) {}
-        try {
-            postOrden(nodo.getRightChild(), pares);
-        } catch (IllegalStateException e) {}
+        
+        if (nodo.hasLeftChild()) {
+            PostOrden(nodo.getLeftChild(), pares);
+        }
+        
+        if (nodo.hasRightChild()) {
+            PostOrden(nodo.getRightChild(), pares);
+        }
+        
         if (nodo.getData() != null && nodo.getData() % 2 == 0) {
             pares.add(nodo.getData());
         }
     }
-      
+    
+    public static void main(String[] args)  {
+        
+        BinaryTree<Integer> arbol = new BinaryTree<>(10);
+        
+        BinaryTree<Integer> leftChild = new BinaryTree<>(5);
+        BinaryTree<Integer> rightChild = new BinaryTree<>(20);
+        
+        arbol.addLeftChild(leftChild);
+        arbol.addRightChild(rightChild);
+        
+        leftChild.addLeftChild(new BinaryTree<>(2));
+        leftChild.addRightChild(new BinaryTree<>(7));
+        
+        rightChild.addLeftChild(new BinaryTree<>(15));
+        
+        ContadorArbol contador = new ContadorArbol(arbol);
+        
+        System.out.println(contador.numerosPares());
+        
+    }
+    
 }
