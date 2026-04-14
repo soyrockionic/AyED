4392621b4c50fa8d16acd1fa1da5b869dd3bb02e
Juan <soyrockionic@gmail.com>
2026-04-14 02:19:45 -0300

    Actualizo ejercicio 4

--- Practicas Resueltas/TP2/Ejercicio4/RedBinariaLlena.java
+++ Practicas Resueltas/TP2/Ejercicio4/RedBinariaLlena.java
@@ -4,24 +4,46 @@ import TP2.Ejercicio1.BinaryTree;
 
 public class RedBinariaLlena {
     
-    private BinaryTree<Integer> red = new BinaryTree();
-
-    public RedBinariaLlena (BinaryTree<Integer> arbol) {
-        this.red = arbol;
-    }
-
-    public int retardoReenvio() {
-        return (!red.isEmpty()) ? retardoReenvio(red) : 0;
+    private BinaryTree<Integer> red = new BinaryTree<>();
+    
+    public RedBinariaLlena (BinaryTree<Integer> red) {
+        this.red = red;
     }
     
-    private int retardoReenvio(BinaryTree<Integer> red) {
+    public int retardoReenvio() {
+        if (this.red == null || this.red.isEmpty()) return 0;
+        return calcularRetardo(red);
+    }
+    
+    private int calcularRetardo(BinaryTree<Integer> red) {
         int retHI = 0;
         int retHD = 0;
         if(red.hasLeftChild())
-            retHI = retardoReenvio(red.getLeftChild());
+            retHI = calcularRetardo(red.getLeftChild());
         if(red.hasRightChild())
-            retHD = retardoReenvio(red.getRightChild());
+            retHD = calcularRetardo(red.getRightChild());
         return (Math.max(retHI, retHD)+ red.getData());
+    }
+    
+    public static void main(String[] args)  {
+        
+        BinaryTree<Integer> arbol = new BinaryTree<>(10);
+        
+        BinaryTree<Integer> leftChild = new BinaryTree<>(2);
+        BinaryTree<Integer> rightChild = new BinaryTree<>(3);
+        
+        arbol.addLeftChild(leftChild);
+        arbol.addRightChild(rightChild);
+        
+        leftChild.addLeftChild(new BinaryTree<Integer>(5));
+        leftChild.addRightChild(new BinaryTree<Integer>(4));
+        
+        rightChild.addLeftChild(new BinaryTree<Integer>(9));
+        rightChild.addRightChild(new BinaryTree<Integer>(8));
+        
+        RedBinariaLlena red = new RedBinariaLlena(arbol);
+        
+        System.out.println(red.retardoReenvio());
     }
     
 }
