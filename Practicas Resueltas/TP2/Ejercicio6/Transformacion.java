02980e7afedfe71657381e94e06a782fdbd15c96
Juan <soyrockionic@gmail.com>
2026-04-14 02:25:53 -0300

    Actualizo ejercicio 6

--- Practicas Resueltas/TP2/Ejercicio6/Transformacion.java
+++ Practicas Resueltas/TP2/Ejercicio6/Transformacion.java
@@ -4,33 +4,57 @@ import TP2.Ejercicio1.BinaryTree;
 
 public class Transformacion {
     
-    private BinaryTree<Integer> arbol = new BinaryTree();
-
-    public Transformacion(BinaryTree<Integer> arbol) {
+    private BinaryTree<Integer> arbol = new BinaryTree<>();
+    
+    public Transformacion (BinaryTree<Integer> arbol) {
         this.arbol = arbol;
     }
-
+    
     public BinaryTree<Integer> suma() {
-        suma(arbol);
+        if (arbol != null && !arbol.isEmpty()) {
+            transformar(arbol);
+        }
         return arbol;
     }
 
-    private int suma(BinaryTree<Integer> nodo) {
+    private int transformar(BinaryTree<Integer> nodo) {
         int sum = 0;
         if(nodo.isLeaf()) {
             sum = nodo.getData();
             nodo.setData(0);
             return sum;
         }
-        if(nodo.hasLeftChild()) {
-            sum+= suma(nodo.getLeftChild());
+        if (nodo.hasLeftChild()) {
+            sum += transformar(nodo.getLeftChild());
         }
-        if(nodo.hasRightChild()) {
-            sum+= suma(nodo.getRightChild());
-        }
+        if (nodo.hasRightChild()) {
+            sum += transformar(nodo.getRightChild());
+        }   
         int actual = nodo.getData();
-            nodo.setData(sum);
+        nodo.setData(sum);
         return actual + sum;
+    }
+    
+    public static void main(String[] args) throws IllegalAccessException  {
+        
+        BinaryTree<Integer> arbol = new BinaryTree<>(10);
+        
+        BinaryTree<Integer> leftChild = new BinaryTree<>(2);
+        BinaryTree<Integer> rightChild = new BinaryTree<>(3);
+        
+        arbol.addLeftChild(leftChild);
+        arbol.addRightChild(rightChild);
+        
+        leftChild.addLeftChild(new BinaryTree<Integer>(5));
+        leftChild.addRightChild(new BinaryTree<Integer>(4));
+        
+        rightChild.addLeftChild(new BinaryTree<Integer>(9));
+        rightChild.addRightChild(new BinaryTree<Integer>(8));
+        
+        Transformacion t = new Transformacion(arbol);
+        arbol.entreNiveles(0, 2);
+        t.suma().entreNiveles(0, 2);
+        
     }
     
 }
