42ac7795f6f6b5147065febdd48dba990e413fca
Juan <soyrockionic@gmail.com>
2026-04-19 22:00:12 -0300

    Actualizo Ejercicio 7

--- Practicas Resueltas/TP3/Ejercicio7/Caminos.java
+++ Practicas Resueltas/TP3/Ejercicio7/Caminos.java
@@ -5,28 +5,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Caminos {
-    private GeneralTree<Integer> arbol; // Suponemos que el árbol se inicializa en el constructor o en otro método.
+    private GeneralTree<Integer> arbol = new GeneralTree<>();
 
     public Caminos(GeneralTree<Integer> arbol) {
         this.arbol = arbol;
     }
     
     public List<Integer> caminoAHojaMasLejana() {
-        return caminoAHojaMasLejana(arbol);
+        return buscarCamino(arbol);
     }
 
-    private List<Integer> caminoAHojaMasLejana(GeneralTree<Integer> nodo) {
+    private List<Integer> buscarCamino(GeneralTree<Integer> nodo) {
         if (nodo == null) {
             return new ArrayList<>();
         }
         List<Integer> caminoMasLargo = new ArrayList<>();
         for (GeneralTree<Integer> hijo : nodo.getChildren()) {
-            List<Integer> caminoActual = caminoAHojaMasLejana(hijo);
+            List<Integer> caminoActual = buscarCamino(hijo);
             if (caminoActual.size() > caminoMasLargo.size()) {
                 caminoMasLargo = caminoActual;
             }
         }
-        caminoMasLargo.add(0, nodo.obtenerDato()); // Anteponer el valor del nodo actual a la ruta
+        // Anteponer el valor del nodo actual a la ruta
+        caminoMasLargo.add(0, nodo.getData());
         return caminoMasLargo;
     }
     
