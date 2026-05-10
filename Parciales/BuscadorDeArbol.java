5c6048f591dd74aae9c520cafb0ed95fd1165a30
Juan <soyrockionic@gmail.com>
2026-05-10 00:31:41 -0300

    Subi mas Parciales

---
+++ Parciales/BuscadorDeArbol.java
@@ -0,0 +1,86 @@
+package tp3.ejercicio1;
+
+/*
+Dada una clase denominada BuscadorDeArbol, que tiene como variable de instancia
+un ArbolGeneral<Integer> denominado arbol implemente en dicha clase el metodo:
+public Integer buscarMayorEnPostOrden() que debe retornar el elemento mayor.
+
+Ejemplo en el siguiente arbol debe retornar el 22.
+
+2
+=> 7
+    => 4
+    => 6
+=> 5
+    => 9
+=> 22
+    => 20
+    => 3
+
+- Debe respetar la clase y el metoo indicado.
+- Puede definir todos los metodos y variables auxiliares que considere.
+- Todo metodo que no este definido en las practicas debe ser implementado.
+- Respetar el recorrido solicitado.
+*/
+
+public class BuscadorDeArbol {
+    
+    private GeneralTree<Integer> arbol = new GeneralTree<>();
+    
+    public BuscadorDeArbol (GeneralTree<Integer> arbol) {
+        this.arbol = arbol;
+    }
+    
+    public Integer buscarMayorEnPostOrden() {
+        return buscarMaximo(arbol);
+    }
+    
+    private Integer buscarMaximo (GeneralTree<Integer> nodo) {
+        if (nodo == null || nodo.isEmpty()) return null;
+        
+        Integer max = nodo.getData();
+        
+        for (GeneralTree<Integer> hijo : nodo.getChildren()) {
+            Integer maxActual = buscarMaximo(hijo);
+            if (maxActual != null && maxActual > max) {
+                max = maxActual;
+            }
+        }
+        
+        return max;
+    }
+    
+    public static void main(String[] args) {
+        
+        GeneralTree<Integer> arbol = new GeneralTree<>(2);
+        
+        GeneralTree<Integer> hijo1 = new GeneralTree<>(7);
+        GeneralTree<Integer> hijo2 = new GeneralTree<>(5);
+        GeneralTree<Integer> hijo3 = new GeneralTree<>(22);
+        
+        arbol.addChild(hijo1);
+        arbol.addChild(hijo2);
+        arbol.addChild(hijo3);
+        
+        GeneralTree<Integer> bisnieto1 = new GeneralTree<>(4);
+        GeneralTree<Integer> bisnieto2 = new GeneralTree<>(6);
+        
+        hijo1.addChild(bisnieto1);
+        hijo1.addChild(bisnieto2);
+        
+        GeneralTree<Integer> bisnieto3 = new GeneralTree<>(9);
+        
+        hijo2.addChild(bisnieto3);
+        
+        GeneralTree<Integer> bisnieto4 = new GeneralTree<>(20);
+        GeneralTree<Integer> bisnieto5 = new GeneralTree<>(3);
+        
+        hijo3.addChild(bisnieto4);
+        hijo3.addChild(bisnieto5);
+        
+        BuscadorDeArbol bda = new BuscadorDeArbol(arbol);
+        System.out.println(bda.buscarMayorEnPostOrden());
+        
+    }
+    
+}
---
+++ Parciales/NivelArbolBinario.java
@@ -0,0 +1,110 @@
+package tp3.ejercicio1;
+
+/*
+Implente en la clase NivelArbolBinario una variable de instancia arbol con un
+BinaryTree<Integer> y un metodo minEnArbolDeAB (int n): BinaryTree<Integer>
+que devuelve el subarbol hoja con menor valor en el nivel n del arbol, De haber
+mas de unodevuelve el primero encontrado. Considere que n es un nivel valido
+del arbol. sin embargo, puede suceder que no existan hojas en ese nivel, en ese
+caso, debe devolver null. Realice un recrrido por niveles.
+
+Para el siguiente arbol:
+
+2
+=> 7
+    => 3
+    => 6
+        => 5
+        => 11
+=> 5
+    =
+    => 9
+        => 4
+
+- Si el nivel es 1, debe devolver null, ya que 7 y 5 no son hojas.
+- Si el nivel es 2, debe devolver 3, ya que es la unica hoja.
+- Si el nivel es 2, debe devolver 4, ya que es el menor entre 5, 11 y 4.
+*/
+
+import java.util.LinkedList;
+import java.util.Queue;
+import tp2.ejercicio1.BinaryTree;
+
+public class NivelArbolBinario {
+     
+    private BinaryTree<Integer> arbol = new BinaryTree <> ();
+    
+    public NivelArbolBinario (BinaryTree<Integer> arbol) {
+        this.arbol = arbol;
+    }
+    
+    public BinaryTree<Integer> minEnArbolDeAB (int n) {
+        if (arbol == null || arbol.isEmpty()) return null;
+       
+        Queue <BinaryTree<Integer>> cola = new LinkedList<>();
+        cola.add(arbol);
+        
+        int nivel = 0;
+        
+        while (!cola.isEmpty()) {
+        
+            int nodosEnNivel = cola.size();
+            
+            if (nivel == n) {
+                BinaryTree<Integer> minHoja = null;
+                for (int i=0; i<nodosEnNivel; i++) {
+                    BinaryTree<Integer> actual = cola.poll();
+                    if (actual.isLeaf()) {
+                        if (minHoja == null || actual.getData() < minHoja.getData()) {
+                            minHoja = actual;
+                        }
+                    }
+                }
+                return minHoja;
+            }
+            
+            for (int i=0; i<nodosEnNivel; i++) {
+                BinaryTree<Integer> nodo = cola.poll();
+                if (nodo.hasLeftChild()) cola.add(nodo.getLeftChild());
+                if (nodo.hasRightChild()) cola.add(nodo.getRightChild());
+            }
+            nivel++;
+        }
+        
+        return null;
+    }
+    
+    public static void main(String[] args) {
+        
+        BinaryTree<Integer> arbol = new BinaryTree <> (2);
+        
+        BinaryTree<Integer> hijo1 = new BinaryTree <> (7);
+        BinaryTree<Integer> hijo2 = new BinaryTree <> (5);
+        
+        arbol.addLeftChild(hijo1);
+        arbol.addRightChild(hijo2);
+        
+        BinaryTree<Integer> nieto1 = new BinaryTree <> (3);
+        BinaryTree<Integer> nieto2 = new BinaryTree <> (6);
+        BinaryTree<Integer> nieto3 = new BinaryTree <> (9);
+        
+        hijo1.addLeftChild(nieto1);
+        hijo1.addRightChild(nieto2);
+        
+        hijo2.addRightChild(nieto3);
+        
+        BinaryTree<Integer> bisnieto1 = new BinaryTree <> (5);
+        BinaryTree<Integer> bisnieto2 = new BinaryTree <> (11);
+        BinaryTree<Integer> bisnieto3 = new BinaryTree <> (4);
+        
+        nieto2.addLeftChild(bisnieto1);
+        nieto2.addRightChild(bisnieto2);
+        
+        nieto3.addLeftChild(bisnieto3);
+        
+        NivelArbolBinario nab = new NivelArbolBinario (arbol);
+        System.out.println(nab.minEnArbolDeAB(2));
+        
+    }
+    
+}
